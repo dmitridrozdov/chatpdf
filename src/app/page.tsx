b977401256f1ee09ca2763aa4f4f4fc191592527
dmitridrozdov <dm.drozdov@gmail.com>
2023-09-15 15:31:51 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>hello josy</h1>
+    <Button>rocky</Button>
   )
 }
